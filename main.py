4678f365627254cac7d95b0540a6c57e4b7ad1d7
Linux0Hat <adrien@d4x.fr>
2024-12-07 16:26:49 +0100

    kayou

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from microdot import Microdot
+from pyb import UART
 import network
 
 wlan = network.WLAN(network.STA_IF)
@@ -6,12 +8,21 @@ ssid ='SALLE_ADO'
 password ='plcbAT2023'
 wlan.connect(ssid, password)
 
-from microdot import Microdot
-
 app = Microdot()
+
+def render(content):
+    return '<meta charset="utf-8">{0}'.format(content), 200, {"Content-Type":"text/html"} 
 
 @app.route('/')
 async def index(request):
-    return 'kayooouuu'
+    return render('<h1>🪨KAYOOOUUU🪨</h1>')
 
-app.run(host="0.0.0.0", port=5000)
+@app.route('/api/kayou')
+async def kayou(request):
+    uart = UART(1, 9600)
+    uart.write('kayou')
+    return '', 200
+
+port = 5000
+print("Starting microdot on port {0}".format(port))
+app.run(host="0.0.0.0", port=port)
